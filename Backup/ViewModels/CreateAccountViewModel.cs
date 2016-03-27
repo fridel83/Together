0907601dcf2a1eb12090fd0da99aaadc57e9a4fb
idriss <idriss.eliguene@gmail.com>
2016-03-27 09:17:49 +0200

    Maj csproj
    Change-Id: Ia2490fd41eb2ca43de527d64e55f6aea24e398b3

---
+++ Backup/ViewModels/CreateAccountViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using RoadBetterTogether.Models.ValidationForm;
+
+namespace RoadBetterTogether.ViewModels
+{
+    public class CreateAccountViewModel
+    {
+        public TogetherUsersSet user { get; set; }
+        public AdressesSet home { get; set; } 
+        public AdressesSet work { get; set; } 
+        //public TogetherCarsSet car { get; set; } 
+    }
+}
